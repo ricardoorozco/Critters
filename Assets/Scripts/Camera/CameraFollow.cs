6f38af53660436c89243328103981a9baaf5fcf0
Ricardo Orozco Leon <orozcoleonricardo@gmail.com>
2017-08-08 05:06:08 -0500

    Se adiciona funcion de ayuda de camara y se colocan las paredes del nivel 1

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
 
+    //private variable to store a reference to the player game object
+	[SerializeField] private GameObject player;
+    //hight to change for help button
+    [SerializeField] private float hight = 1.91f;
+    //help time in seconds
+    [SerializeField] private int helpTime = 5;
 
-	[SerializeField] private GameObject player;       //Public variable to store a reference to the player game object
-
-	// LateUpdate is called after Update each frame
-	void LateUpdate () 
+    // LateUpdate is called after Update each frame
+    void LateUpdate () 
 	{
-		transform.position = new Vector3(player.transform.position.x,transform.position.y,player.transform.position.z);
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        
 	}
+
+    public void startHelpCam() {
+        StartCoroutine("HelpCam");
+    }
+
+    IEnumerator HelpCam() {
+        transform.position = new Vector3(player.transform.position.x, hight+10, player.transform.position.z);
+        yield return new WaitForSeconds(helpTime);
+        transform.position = new Vector3(player.transform.position.x, hight, player.transform.position.z);
+        
+    }
 }
